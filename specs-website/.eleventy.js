be17d9c8fa693c77dcd02cbf5ffe7657b6217b65
bengo <171782+gobengo@users.noreply.github.com>
2024-01-18 09:18:47 -0800

    fix build

--- specs-website/.eleventy.js
+++ specs-website/.eleventy.js
@@ -2,11 +2,5 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const path = require('path')
 
 module.exports = function(eleventyConfig) {
-	const dir = path.resolve(__dirname, '../')
-
 	eleventyConfig.addPlugin(pluginWebc);
-
-	return {
-		dir,
-	}
 };
